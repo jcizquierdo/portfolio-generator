20d55dda0e77709cb86369d5db3b2911d4a87e71
JC Izquierdo <josecarlosizquierdo1994@gmail.com>
2020-05-25 22:22:50 -0500

    added inquirer to app.js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(portfolioData);
+// const profileDataArgs = process.argv.slice(2);
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const [name, github] = profileDataArgs;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+const inquirer = require('inquirer');
+console.log(inquirer);
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
